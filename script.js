bd7d7ff38367a4c8b91ef37a29f2a92fe9dab8b7
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-09-14 22:33:23 +0600

    start typing after 0 completed successfully

--- script.js
+++ script.js
@@ -108,14 +108,13 @@ const start = () => {
 
   const startCountdown = setInterval(() => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
-
     // finished timer
-    if (count == 0) {
+    if (count === 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "flex";
       display.classList.remove("inactive");
-
+      countdownOverlay.style.display = "none";
       clearInterval(startCountdown);
       startTime = new Date().getTime();
     }
